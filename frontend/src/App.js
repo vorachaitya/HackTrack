7b3ca6cad9e3f3ec890dc611622b78644babfbb1
Chaitya <chaityavora02@gmail.com>
2023-02-17 20:02:58 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navb from './components/Navb';
 import styles from "./index.css"
+import { Notifications } from 'react-push-notification';
+import Notify from './components/Notify';
+import Body from './components/404notFound/Body';
+
 // import Admin from './components/Admin';
 // import Timeline from './components/Timeline';
 // import Video from './components/Video';
@@ -11,13 +15,17 @@ import styles from "./index.css"
 function App() {
   return (
     <>
-    
+    <Body/>
+      <Notifications />
+      <Notify/>
     <BrowserRouter>
      <Navb/>
      {/* <Home/> */}
         <Routes>
           <Route exact path='/' element={<Home />} />
-         
+          {/* <Route path="/signup" exact element={<Signup />} />
+                
+          <Route path="/login" exact element={<Login />} /> */}
           {/* <Route exact path='/video' element={<Video />} /> */}
 
         </Routes>
