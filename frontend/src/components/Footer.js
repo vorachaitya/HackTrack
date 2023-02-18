511b0e35af4a26aa979909bd43bb4eef8c572515
Kunal Agarwal <kunalagarwal1072002@gmail.com>
2023-02-18 09:40:27 +0530

    Added footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,48 +8,48 @@ const Footer = () => {
     <div className="mb-30 col-md-6 col-lg-4">
       <div className="card">
       <img className="card-icon" src="https://image.ibb.co/cFV8mR/monitoring.png" alt="monitoring" />
-      <h3 className="card-title">Monitoring</h3>
-      <p className="card-text">Collect metrics on visibility, monitor Droplet performance and receive alerts when problems arise in your infrastructure–at no additional cost.</p>
+      <h3 className="card-title">Resume Parsing</h3>
+      <p className="card-text">Uploaded resume's of applicants will be parsed and a score would be generated on basis of parsing algorithm. Teams with higher score can be given preferences.</p>
       <a className="card-link" href="#">Learn more</a>
       </div>
     </div>
     <div className="mb-30 col-md-6 col-lg-4">
       <div className="card">
       <img className="card-icon" src="https://image.ibb.co/jfmg6R/cloud_firewalls.png" alt="monitoring" />
-      <h3 className="card-title">Cloud Firewalls</h3>
-      <p className="card-text">Perfect for both staging and production deployments, cloud firewalls let you easily secure your infrastructure and define what services are visible on your Droplets–at no additional cost.</p>
+      <h3 className="card-title">Video Conference</h3>
+      <p className="card-text">Admin can have hassle free one-to-one live video conference with candidates featuring high picture quality and audio sound.Candidates can also have inter calls with their peers.</p>
       <a className="card-link" href="#">Learn more</a>
       </div>
     </div>
     <div className="mb-30 col-md-6 col-lg-4">
       <div className="card">
       <img className="card-icon" src="https://image.ibb.co/fcnzt6/team_management.png" alt="team management" />
-      <h3 className="card-title">Team Management</h3>
-      <p className="card-text">With Digital Teams you can invite and manage users, ensure security with two-factor authentication, and control your resource spend with centralized billing–at no additional cost.</p>
+      <h3 className="card-title">Chat Application</h3>
+      <p className="card-text">In order to team up with other candidates,they can have communication via chat application.Even admin can use whenever needed.</p>
       <a className="card-link" href="#">Learn more</a>
       </div>
     </div>
     <div className="mb-30 col-md-6 col-lg-4">
       <div className="card">
-      <img className="card-icon" src="https://image.ibb.co/evyiLm/backups.png" alt="monitoring" />
-      <h3 className="card-title">Backups</h3>
-      <p className="card-text">Backups run automatically on your Droplets each week and are stored for four weeks–add 20% of monthly Droplet cost.</p>
-      <a className="card-link" href="#">Learn more</a>
-      </div>
-    </div>
-    <div className="mb-30 col-md-6 col-lg-4">
-      <div className="card">
-      <img className="card-icon" src="https://image.ibb.co/g9bERR/snapshots.png" alt="snapshots" />
-      <h3 className="card-title">Snapshots</h3>
-      <p className="card-text">Scale out your system effortlessly. Just take a snapshot of an existing server, then spin up a new Droplet from the snapshot–add $0.05 per gigabyte per month.</p>
+      <img className="card-icon" src="https://image.ibb.co/fcnzt6/team_management.png" alt="team management" />
+      <h3 className="card-title">Candidate Analysis</h3>
+      <p className="card-text">Candidate can view his resume score,get recommendation for upgradation of his skills,compare the competition via studying graph created on basis of domain selection,experience level and skills.</p>
       <a className="card-link" href="#">Learn more</a>
       </div>
     </div>
     <div className="mb-30 col-md-6 col-lg-4">
       <div className="card">
       <img className="card-icon" src="https://image.ibb.co/cFV8mR/monitoring.png" alt="monitoring" />
-      <h3 className="card-title">Monitoring</h3>
-      <p className="card-text">Collect metrics on visibility, monitor Droplet performance and receive alerts when problems arise in your infrastructure–at no additional cost.</p>
+      <h3 className="card-title">Timeline Notifications</h3>
+      <p className="card-text">To have smooth conduct of hackathon, admins can keep the registered candidates updated by posting important announcements on Timeline tool.</p>
+      <a className="card-link" href="#">Learn more</a>
+      </div>
+    </div>
+    <div className="mb-30 col-md-6 col-lg-4">
+      <div className="card">
+      <img className="card-icon" src="https://image.ibb.co/evyiLm/backups.png" alt="monitoring" />
+      <h3 className="card-title">Student Track</h3>
+      <p className="card-text">You can keep track of registered students via database, filter them out easily out of large volume of applicants. </p>
       <a className="card-link" href="#">Learn more</a>
       </div>
     </div>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 
 import './css/home.css'
+import Footer from './Footer';
 import Navi from './Navi';
 const Home = () => {
   return (
@@ -23,13 +24,14 @@ const Home = () => {
 <section className="banner">
   <div className="container">
     <div className="banner-text">
-      <h1>Manage</h1>
-      <p><strong>We make it easy to get the most out of your virtual machine.</strong> Our tools make it simple to track how your Droplets are performing and help you to respond quickly if there is a problem</p>
-      <button>Sign Up</button>
+      <h1>Hack Track</h1>
+      <p>We make it easy to streamline the process of shortlisting teams for Hackathons. Our tools along with it's user friendly features make it simple to select best teams from a large volume of applicants for your hackathon</p>
+      <button><a href="http://localhost:8501" style={{color:"white"}}>Sign Up</a></button>
     </div>
   </div>
   <img className="banner-image" src="https://preview.ibb.co/bMi5Y6/banner_img.png" alt="monitoring" />
 </section>
+<Footer/>
 
 
     </>
